a8184054adce9847f4380fa2498d51d00d86333c
user <bogyung1@naver.com>
2020-10-26 19:15:25 +0900

    Initial commit

---
+++ whackamole.js
@@ -0,0 +1,62 @@
+const holes = document.querySelectorAll('.hole');
+const scoreBoard = document.querySelector('.score');
+const moles = document.querySelectorAll('.mole');
+let lastHole;
+let timeUp = false;
+let score = 0;
+
+function randTime(min, max) {
+    return Math.round(Math.random()*(max-min)+min)
+
+}
+
+function randHole(holes) {
+    const randIndex=Math.floor(Math.random()*holes.length)
+    const hole=holes[randIndex]
+    if(hole===lastHole){
+        return randHole(holes)
+    }
+
+    lastHole = hole
+    return lastHole
+
+}
+
+function peep() {
+    const time=randTime(1000, 2000)
+    const hole=randHole(holes)
+    hole.classList.add('up')
+
+    setTimeout(()=> {
+        hole.classList.remove('up')
+        if(!timeUp){
+            peep()
+        }
+    },time)
+
+}
+
+function startGame() {
+    scoreBoard.textContent=0
+    score=0
+    timeUp=false
+    console.log("start")
+    peep()
+    console.log("peep")
+    setTimeout(()=>timeUp=true, 10000)
+
+
+
+}
+
+function bonk(e) {
+
+    if(!e.isTrusted) return
+
+    this.classList.remove('up')
+    score++
+    scoreBoard.textContent=score
+
+}
+
+moles.forEach(mole => mole.addEventListener('click', bonk));
